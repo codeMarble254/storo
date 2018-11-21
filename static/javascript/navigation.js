fb840fd00889cd46c8915c33d8cb9f46d66a4dec
Jey <savalijey@gmail.com>
2018-11-21 16:42:00 +0300

    [Feature #162083115] Create the post page

---
+++ static/javascript/navigation.js
@@ -0,0 +1,5 @@
+let loader_holder = document.getElementsByClassName("loader-holder")[0];
+function hideLoader(){
+    document.getElementById("loader").style.display = "none";
+    loader_holder.style.display = "none";
+}
