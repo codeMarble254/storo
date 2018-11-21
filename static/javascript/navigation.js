f92e7af27b3e20d1fcb3bb372fd4be285938c638
Jey <savalijey@gmail.com>
2018-11-21 23:50:20 +0300

    [Feature #162083115] Add all the necessary features to the page

--- static/javascript/navigation.js
+++ static/javascript/navigation.js
@@ -3,3 +3,10 @@ function hideLoader(){
     document.getElementById("loader").style.display = "none";
     loader_holder.style.display = "none";
 }
+
+let options = document.getElementById("options");
+options.addEventListener("click", openOptions);
+function openOptions(){
+    let options_list = document.getElementById("options-list");
+    options_list.style.display = "block";
+}
